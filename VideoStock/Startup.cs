cb8d84538082bb9168737c153c70230c332c7d79
wojciech.kapko <wojciech.kapko@gmail.com>
2021-07-16 22:20:03 +0200

    Rename Api project Add new projects

--- VideoStock/Startup.cs
+++ VideoStock/Startup.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace VideoStock
 {
     public class Startup
@@ -26,7 +27,6 @@ namespace VideoStock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
